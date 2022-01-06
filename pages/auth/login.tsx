0807bf8a07e089d0cb6d8cc983a82c110c7741c6
joon <thedigitalimg@gmail.com>
2022-01-06 18:43:19 +0900

    loading page for /auth/login

---
+++ pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Loader from 'components/Loader';
+
+export default function Login() {
+	return (
+		<div className="w-screen h-screen flex items-center justify-center">
+			<Loader />
+		</div>
+	);
+}

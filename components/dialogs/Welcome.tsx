29a281f397faf0bb93490eb8fbf186250eb4dc04
joon <thedigitalimg@gmail.com>
2022-01-14 23:55:10 +0900

    Welcome message fix

--- components/dialogs/Welcome.tsx
+++ components/dialogs/Welcome.tsx
@@ -6,9 +6,8 @@ export const WelcomePopup = () => {
 	return (
 		<div className="w-full h-full my-auto">
 			<div className="flex flex-col items-center">
-				<h2 className="tracking-wider mb-3">Welcome</h2>
 				<div className="mx-auto max-w-xxxs w-full px-4 mt-4 border border-gray-600 rounded-lg bg-gray-900 py-4">
-					<p className="text-sm">👋 Welcome to Kollider</p>
+					<p className="text-lg">👋 Welcome to Kollider</p>
 					<p className="text-xs pt-2">
 						No need to deposit,
 						<br />

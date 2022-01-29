8e190364bc58b2fc2a425fa21abaa266a7476abd
joon <thedigitalimg@gmail.com>
2022-01-29 21:28:51 +0900

    Only display username when loaded

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -8,10 +8,12 @@ export const UserInfo = () => {
 	const userData = useGetMyUserData();
 	return (
 		<div className="pt-1">
-			<div className="w-full flex items-center justify-between xxs:justify-end gap-6">
-				<p className="text-xs text-gray-400 leading-none">Username</p>
-				<p className="text-[10px] leading-none">{userData?.username}</p>
-			</div>
+			{userData?.username && (
+				<div className="w-full flex items-center justify-between xxs:justify-end gap-6">
+					<p className="text-xs text-gray-400 leading-none">Username</p>
+					<p className="text-[10px] leading-none">{userData?.username}</p>
+				</div>
+			)}
 		</div>
 	);
 };

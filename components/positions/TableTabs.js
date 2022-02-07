4ee3a822871cd2ea8f36e84db255796a1ca13172
the-kollider <info@kollider.xyz>
2022-02-07 23:01:29 +0000

    Made it look prettier

--- components/positions/TableTabs.js
+++ components/positions/TableTabs.js
@@ -7,11 +7,11 @@ export const TableTabs = () => {
 	const [selectedTableTab] = useAppSelector(state => [state.layout.selectedTableTab]);
 	const dispatch = useAppDispatch();
 	return (
-		<div className="flex grid grid-cols-3 bg-gray-900 mb-2 p-2">
-			<div className={cn(selectedTableTab === TABLE_TABS.POSITIONS && 'border-b border-white') + ' cursor-pointer'} onClick={() => dispatch(setTableTab(TABLE_TABS.POSITIONS))}>
+		<div className="flex grid grid-cols-3 bg-gray-900 mb-2 py-2">
+			<div className={cn(selectedTableTab === TABLE_TABS.POSITIONS && 'border-b border-theme-main') + ' cursor-pointer pb-2'} onClick={() => dispatch(setTableTab(TABLE_TABS.POSITIONS))}>
 				Positions
 			</div>
-			<div className={cn(selectedTableTab === TABLE_TABS.TRADES && 'border-b border-white') + ' cursor-pointer'} onClick={() => dispatch(setTableTab(TABLE_TABS.TRADES))}>
+			<div className={cn(selectedTableTab === TABLE_TABS.TRADES && 'border-b border-theme-main') + ' cursor-pointer pb-2'} onClick={() => dispatch(setTableTab(TABLE_TABS.TRADES))}>
 				Trades
 			</div>
 		</div>

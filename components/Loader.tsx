05f3c380f099728b77ccf6fece2f44b61e98bdf0
the-kollider <info@kollider.xyz>
2022-07-25 15:31:22 +0100

    Added custom laoder.

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { TriangleSpinner } from 'components/vendor/spinner';
+import Img from 'react-cool-img';
 
 export default function Loader({ text }: { text?: string }) {
 	return (
@@ -9,12 +10,12 @@ export default function Loader({ text }: { text?: string }) {
 				{text ? (
 					<div className="grid grid-rows-2 gap-4">
 						<div className="flex justify-center">
-							<TriangleSpinner color="white" height={50} width={50} />
+							<Img src={'kollider_loader.svg'} height={126} width={126} />
 						</div>
 						<div className="text-center">{text}</div>
 					</div>
 				) : (
-					<TriangleSpinner color="white" height={50} width={50} />
+					<Img src={'kollider_loader.svg'} height={126} width={126} />
 				)}
 			</div>
 		</div>
